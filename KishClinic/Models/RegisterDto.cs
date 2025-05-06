183fa1ad6f75d4a3220b1f6d543e20bf8fd24594
kapop <kapopenko@gmail.com>
2025-05-06 21:10:35 +0300

    Add LoginDto and rename UserDto to RegisterDto

--- KishClinic/Models/RegisterDto.cs
+++ KishClinic/Models/RegisterDto.cs
@@ -1,11 +1,11 @@
 ﻿namespace KishClinic.Models
 {
-    public class UserDto
+    public class RegisterDto
     { 
         public string Email { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public DateOnly? DateOfBirth { get; set; }
+        public DateOnly DateOfBirth { get; set; }
         public string Phone { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string Notes { get; set; } = string.Empty;
